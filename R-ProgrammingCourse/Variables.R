0743bbb5ab0ab8faa0ea4d6a0923e1042f4cf54b
deepak-raghavendra-bhat <deepak.raghavendra.bhat@outlook.com>
2023-03-10 10:27:30 +0530

    Variables and using them

---
+++ R-ProgrammingCourse/Variables.R
@@ -0,0 +1,38 @@
+#integer
+
+x <- 2L
+typeof(x)
+
+x1 <- 10
+typeof(x1)
+
+
+
+#double
+
+y <- 3.24
+typeof(y)
+
+#complex
+
+z = 3 + 2i
+typeof(z)
+
+#character
+
+a = "H"
+typeof(a)
+
+b <- a
+typeof(b)
+
+#logical
+
+q1 <- TRUE
+typeof(q1)
+
+q2 <- FALSE
+typeof(q2)
+
+
+
