1f06a1c065f3070ad6799dbf71147846215fa15a
deepak-raghavendra-bhat <deepak.raghavendra.bhat@outlook.com>
2023-04-06 11:22:57 +0530

    Changed file locations for better visibility

---
+++ R-ProgrammingCourse/Section-1/Variables.R
@@ -0,0 +1,64 @@
+#integer
+
+x <- 2L
+typeof(x)
+
+x1 <- 10
+typeof(x1)
+
+
+
+#double
+
+y <- 3.24
+typeof(y)
+
+#complex
+
+z = 3 + 2i
+typeof(z)
+
+#character
+
+a = "H"
+typeof(a)
+
+b <- a
+typeof(b)
+
+#logical
+
+q1 <- TRUE
+typeof(q1)
+
+q2 <- FALSE
+typeof(q2)
+
+#using variables
+
+
+#addition of vars
+
+A <- 10
+B <- 5
+
+C <- A + B
+typeof(C)
+print(C)
+
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1/var2
+result
+
+answer <- sqrt(var2)
+print(answer)
+
+greeting <- "Hello"
+name <- "Deepak"
+
+message <- paste(greeting,name)
+message
+
