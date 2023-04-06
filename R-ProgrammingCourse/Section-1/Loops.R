b1b4ffa5ae189bb7c66a26a526054fd56df66ee3
deepak-raghavendra-bhat <deepak.raghavendra.bhat@outlook.com>
2023-04-06 12:06:58 +0530

    while loop tutorials

---
+++ R-ProgrammingCourse/Section-1/Loops.R
@@ -0,0 +1,25 @@
+# while loops
+
+a <- 4
+b <- "Hello"
+while (a > 1) {
+  
+  print(b)
+  a <- a - 1
+  
+}
+
+while(TRUE)
+{
+  print("HELLO")
+}
+
+counter <- 1
+while(counter < 12)
+{
+  print(counter)
+  counter <- counter + 1
+}
+
+
+
