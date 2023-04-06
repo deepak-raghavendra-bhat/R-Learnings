fc18315d746c560d3ebfc933d9dfaa59548df073
deepak-raghavendra-bhat <deepak.raghavendra.bhat@outlook.com>
2023-04-06 11:57:52 +0530

    R Logical Operators

---
+++ R-ProgrammingCourse/Section-1/LogicalOperators.R
@@ -0,0 +1,43 @@
+#logical
+#true
+#false
+
+4 < 5
+10 > 100
+
+# ==
+# !=
+# <
+# >
+# <=
+# >=
+# !
+# |
+# &
+# isTRUE(x)
+
+res <- 4 < 5
+res
+
+print(res)
+
+typeof(res)
+
+a <- !TRUE
+a
+
+a <- !(4 < 5)
+a
+
+TRUE | TRUE
+TRUE | FALSE
+FALSE | FALSE
+
+TRUE & TRUE
+TRUE & FALSE
+FALSE & FALSE
+
+isTRUE(4 < 5)
+
+isTRUE(40 < 5)
+
